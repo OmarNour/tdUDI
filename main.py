45b9d6decd2948d5367e840754d0a032302cfdb6
Omar Nour <omar_nour@hotmail.com>
2023-01-21 22:24:24 +0200

    dev

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ def start():
     smx.parse_file()
     smx.extract_all()
 
+    print('DataSetType count:', len(DataSetType.get_instance()))
     print('Layer count:', len(Layer.get_instance()))
     print('Schema count:', len(Schema.get_instance()))
+    print('DataType count:', len(DataType.get_instance()))
     print('DataSource count:', len(DataSource.get_instance()))
     print('Table count:', len(Table.get_instance()))
     print('LayerTable count:', len(LayerTable.get_instance()))
-    print('DataSetType count:', len(DataSetType.get_instance()))
     print('DataSet count:', len(DataSet.get_instance()))
     print('Domain count:', len(Domain.get_instance()))
     print('DomainValue count:', len(DomainValue.get_instance()))
-    print('DataType count:', len(DataType.get_instance()))
     print('Column count:', len(Column.get_instance()))
 
 
