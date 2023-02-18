b9f075337a8e5336f33a1e7ffc63253fcf6a250e
Omar Nour <omar_nour@hotmail.com>
2023-02-18 12:08:14 +0200

    dev

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from client.smx import *
 
-
+# TODO:
+#   raise error if trx is in valid for columns mapping or in where clauses
 @time_elapsed_decorator
 def start():
     smx = SMX(smx_path, scripts_path)
