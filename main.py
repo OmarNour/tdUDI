03dd8a21bab781f69f6a5d326100a21e1739bb5a
Omar Nour <omar_nour@hotmail.com>
2023-03-19 13:01:34 +0200

    dev

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ def start(source_name: str|list|None, with_scripts=True):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     try:
-        start(source_name=None)
+        start(source_name='cso')
     except KeyboardInterrupt:
         print("Ops!..")
