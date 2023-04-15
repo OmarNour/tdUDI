9e19fb77a49ffd63ac0d3508dad4fd1870dd2321
Omar Nour <omar_nour@hotmail.com>
2023-04-15 18:22:48 +0200

    dev

--- client/teradata_objects/ETL_EGINE/35_COUNT_ROWS.sql
+++ client/teradata_objects/ETL_EGINE/35_COUNT_ROWS.sql
@@ -2,6 +2,7 @@ REPLACE PROCEDURE /*VER.1*/ GDEV1P_PP.COUNT_ROWS
 (
 	in I_DB 			VARCHAR (200),
 	in I_TBL 			VARCHAR (200),
+	in I_QUERY 			VARCHAR (5000),
 	out O_CNT 			FLOAT,
 	out O_RETURN_CODE 	INTEGER,
 	out O_RETURN_MSG 	VARCHAR (5000)
@@ -10,9 +11,9 @@ BEGIN
 	
 	DECLARE C1 CURSOR FOR S1;
 	BEGIN
-		DECLARE V_TABLE_NAME 	VARCHAR(1000) DEFAULT '';
-		DECLARE v_cnt_query 	VARCHAR(1000);
-		DECLARE v_cnt_result 	FLOAT DEFAULT 0;
+		DECLARE V_TABLE_NAME 	VARCHAR(500) DEFAULT '';
+		DECLARE v_cnt_query_stmt, v_cnt_tbl_stmt 	VARCHAR(5000);
+		DECLARE v_cnt_qry, v_cnt_tbl 	FLOAT DEFAULT 0;
 		
 		DECLARE EXIT HANDLER FOR SQLEXCEPTION
 		BEGIN
@@ -29,14 +30,30 @@ BEGIN
 			ELSE
 				SET V_TABLE_NAME = I_TBL;
 			END IF;
+			
+			SET v_cnt_tbl_stmt = 'SEL COUNT (1) (float) FROM ' ||V_TABLE_NAME ||';' ;
+			
+			PREPARE S1 FROM v_cnt_tbl_stmt;
+			OPEN C1;
+			FETCH C1 INTO v_cnt_tbl;
+			CLOSE C1;
+			
 		END IF;
 		
-		SET v_cnt_query = 'SEL COUNT (1) (float) FROM ' ||V_TABLE_NAME ||';' ;
-		PREPARE S1 FROM v_cnt_query;
-		OPEN C1;
-		FETCH C1 INTO v_cnt_result;
-		CLOSE C1;
-		SET O_CNT = v_cnt_result;
+		IF COALESCE (I_QUERY, '') <>'' 
+		THEN
+			
+			SET v_cnt_query_stmt = 'SEL COUNT (1) (float) FROM (' ||I_QUERY ||') x;' ;
+			
+			PREPARE S1 FROM v_cnt_query_stmt;
+			OPEN C1;
+			FETCH C1 INTO v_cnt_qry;
+			CLOSE C1;
+			
+		END IF;
+		
+		
+		SET O_CNT = v_cnt_tbl + v_cnt_qry;
 		SET O_RETURN_CODE = 0;
 		SET O_RETURN_MSG = 'Process Completed Successfully';
 	END;
--- client/teradata_objects/ETL_EGINE/50_STG_PROCESS_LOADING.sql
+++ client/teradata_objects/ETL_EGINE/50_STG_PROCESS_LOADING.sql
@@ -16,7 +16,8 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1P_PP.STG_PROCESS_LOADING
         DECLARE 	V_DBC_RETURN_CODE 			INTEGER;
 		DECLARE 	V_DBC_RETURN_MSG  			VARCHAR(1000);
 		DECLARE 	V_DBC_ROWS_COUNT			FLOAT;
-    	DECLARE 	v_statment					CLOB;                                
+    	DECLARE 	v_drop_1batch
+    				, v_create_1batch			CLOB;                                
         DECLARE    	V_SRC_DB					VARCHAR(500);
         DECLARE    	V_SRC_TABLE					VARCHAR(500);
         DECLARE    	V_TGT_DB					VARCHAR(500);
@@ -27,19 +28,24 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1P_PP.STG_PROCESS_LOADING
         DECLARE		V_BATCH_ID					VARCHAR(150)	DEFAULT 'BATCH_ID';
         DECLARE	   	V_REF_KEY   				VARCHAR(150)	DEFAULT 'REF_KEY';
         DECLARE	   	V_MODIFICATION_TYPE 		VARCHAR(150)	DEFAULT 'MODIFICATION_TYPE';
-        
+        DECLARE		V_STAGING_DB				VARCHAR(150)	DEFAULT 'GDEV1T_STG';
      	
         DECLARE 	V_WITH_DATA_PI				VARCHAR(100) DEFAULT 'WITH DATA AND STATS PRIMARY INDEX';
 		DECLARE 	V_WITH_DATA_UNIQUE_PI		VARCHAR(100) DEFAULT 'WITH DATA AND STATS UNIQUE PRIMARY INDEX';
 	
 		DECLARE		v_table_name_1batch,
 					V_SOURCE_NAME,
-				    V_SOURCE_MODE,
+				    V_LOADING_MODE,
 				    V_REJECTION_TABLE_NAME,
 				    V_BUSINESS_RULES_TABLE_NAME,
 				    V_SOURCE_DB					VARCHAR(500);
 				    
 		DECLARE		V_IS_TARANSACTIOANL			INTEGER;
+		declare		v_online_load_id_filter,
+					v_valid_batches_filter,
+					V_UPDATE_ALL_TO_D,
+					V_KEY_COLs, v_, v_keys_eql,
+					v_valid_data_filter			varchar(2000) default '';
 		
         DECLARE EXIT HANDLER FOR SQLEXCEPTION
         BEGIN 	
@@ -58,7 +64,7 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1P_PP.STG_PROCESS_LOADING
         BEGIN
             SELECT 
             SRC.SOURCE_NAME,
-		    SRC.SOURCE_MODE,
+		    SRC.LOADING_MODE,
 		    SRC.REJECTION_TABLE_NAME,
 		    SRC.BUSINESS_RULES_TABLE_NAME,
 		    SRC.SOURCE_DB,
@@ -73,22 +79,128 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1P_PP.STG_PROCESS_LOADING
 		    AND  TBL.TABLE_NAME = i_TABLE_NAME
 		    
 		    INTO V_SOURCE_NAME,
-		    V_SOURCE_MODE,
+		    V_LOADING_MODE,
 		    V_REJECTION_TABLE_NAME,
 		    V_BUSINESS_RULES_TABLE_NAME,
 		    V_SOURCE_DB,
 		    V_IS_TARANSACTIOANL;
             
-		    set v_table_name_1batch = i_TABLE_NAME||'_1batch';
-		    
-            IF V_SOURCE_NAME IS NULL
+		    IF V_SOURCE_NAME IS NULL
             THEN
             	set V_RETURN_CODE = -1;
             	SET V_RETURN_MSG = 'Invalid Proccess!';
             	leave MAINBLOCK;
             end if;
             
+            SELECT 
+			 TRIM( TRAILING  ',' FROM (XMLAGG(trim(Key_Column)|| ',' ORDER BY Key_Column) (VARCHAR(10000)))) Key_Column
+			, trim(XMLAGG( 'TGT.' || trim(Key_Column)|| ' = SRC.' ||trim(Key_Column) || ' AND ' ORDER BY Key_Column) (VARCHAR(10000))) _keys_eql
+			, left(_keys_eql,(length(_keys_eql)-3)(int)) Keys_eql
+			from GDEV1t_GCFR.TRANSFORM_KEYCOL
+			WHERE DB_NAME = V_STAGING_DB
+			AND TABLE_NAME  = i_TABLE_NAME
+			INTO V_KEY_COLs, v_, v_keys_eql;
+			
+			if coalesce(V_KEY_COLs,'') is null 
+			then
+				set V_RETURN_CODE = -1;
+            	SET V_RETURN_MSG = 'No keys defined for table '||i_TABLE_NAME||'!';
+            	leave MAINBLOCK;
+			end if;
+			
+		
+		    set v_table_name_1batch = i_TABLE_NAME||'_1batch';
+		    if V_REJECTION_TABLE_NAME is not null and V_BUSINESS_RULES_TABLE_NAME is not null
+		    then
+			    set v_valid_data_filter = 
+					'and not exists
+						(
+							sel 1 
+							from '||V_SOURCE_DB||'.'||V_REJECTION_TABLE_NAME||' r  
+							where r.REF_KEY  = x.REF_KEY
+							and exists (
+											select 1 
+											from '||V_SOURCE_DB||'.'||V_BUSINESS_RULES_TABLE_NAME||' br 
+											where r.RULE_ID = br.RULE_ID 
+											and br.active = 1 /*hard rejection rules*/							
+										)
+						)';
+			end if;
             
+            if V_LOADING_MODE = 'ONLINE'
+            then
+            	if i_LOAD_ID is not null 
+				then
+					set v_online_load_id_filter = ' and x.LOAD_ID = '''||i_LOAD_ID||'''';		
+				end if;
+				
+				if i_LOAD_ID = 'RELOAD_DONE'
+				then
+					set v_online_load_id_filter = ' and exists (
+																select 1 
+																from GDEV1t_GCFR.ONLINE_RUN_CONFIG c 
+																where c.LOAD_ID = x.LOAD_ID 
+																and c.STATUS <> ''DELETED''
+																and c.STG_status = ''DONE''
+																)';		
+				end if;
+				
+				set v_valid_batches_filter = 
+						' and exists 
+						(
+							SELECT 1
+							FROM '||V_SOURCE_DB||'.ibm_audit_table adt
+							WHERE adt.DB2_TO_TERA_STATUS in (''SUCCESS'', ''SUCCESS WITH WARNINGS'')
+							AND adt.DB2_JOB_END_TIME IS NOT NULL  
+							AND adt.MIC_EXECUTIONGUID = x.LOAD_ID
+							AND adt.MIC_RUNID = x.BATCH_ID
+							AND exists (
+											select 1 
+											from GDEV1t_GCFR.SOURCE_NAME_LKP lkp
+											where lkp.teradata_SOURCE_NAME = '''||V_SOURCE_DB||'''
+											and lkp.MICROSOFT_SOURCE_NAME = adt.MIC_GANAME
+										)
+						)';	
+					
+            else
+            	SET V_UPDATE_ALL_TO_D = 'update '||V_STAGING_DB||'.'||i_TABLE_NAME||' set modification_type = ''D''; ';
+            end if;
+            
+			
+            set v_drop_1batch = 'DROP TABLE '||v_table_name_1batch||';';
+            set v_create_1batch = 
+			'create multiset volatile TABLE '||v_table_name_1batch||'  , no fallback as 
+			(
+				sel * from '||V_SOURCE_DB||'.'||i_TABLE_NAME||' x
+				where 1=1
+				'||v_valid_data_filter||'
+				'||v_valid_batches_filter||' 		
+				'||v_online_load_id_filter||' 
+				QUALIFY ROW_NUMBER()  OVER(PARTITION BY '||V_KEY_COLs||' ORDER BY BATCH_ID DESC,MODIFICATION_TYPE DESC,REF_KEY DESC)=1
+			) with data and stats primary index ('||V_KEY_COLs||') on commit preserve rows;';
+			
+			set v_update_tgt = 
+							'update tgt
+							from '||I_STAGING_DB||'.'||i_TABLE_NAME||' tgt, '||v_table_name_1batch||' src
+							set '||v_set_non_key_cols||', '||v_UPDT_MODIFICATION_TYPE_COL||'
+							where '||v_keys_eql||';		
+							';
+							
+            SET V_SQL_SCRIPT_ID = V_SQL_SCRIPT_ID + 1;
+			CALL GDEV1P_PP.DBC_SYSEXECSQL_WITH_LOG(v_drop_1batch,V_SQL_SCRIPT_ID,v_run_id,V_SOURCE_NAME,i_TABLE_NAME,I_LOAD_ID,1/*1 log or 0 don't*/,V_DBC_RETURN_CODE,V_DBC_RETURN_MSG,V_DBC_ROWS_COUNT);
+			IF V_DBC_RETURN_CODE <> 0 THEN
+				SET V_RETURN_CODE = V_DBC_RETURN_CODE;
+				SET V_RETURN_MSG = V_DBC_RETURN_MSG;
+				LEAVE MainBlock;
+			END IF;
+			
+			SET V_SQL_SCRIPT_ID = V_SQL_SCRIPT_ID + 1;
+			CALL GDEV1P_PP.DBC_SYSEXECSQL_WITH_LOG(v_create_1batch,V_SQL_SCRIPT_ID,v_run_id,V_SOURCE_NAME,i_TABLE_NAME,I_LOAD_ID,1/*1 log or 0 don't*/,V_DBC_RETURN_CODE,V_DBC_RETURN_MSG,V_DBC_ROWS_COUNT);
+			IF V_DBC_RETURN_CODE <> 0 THEN
+				SET V_RETURN_CODE = V_DBC_RETURN_CODE;
+				SET V_RETURN_MSG = V_DBC_RETURN_MSG;
+				LEAVE MainBlock;
+			END IF;
 			
 			SET V_SQL_SCRIPT_ID = V_SQL_SCRIPT_ID + 1;
 			CALL GDEV1P_PP.DBC_SYSEXECSQL_WITH_LOG('BT',V_SQL_SCRIPT_ID,v_run_id,V_SOURCE_NAME,i_TABLE_NAME,I_LOAD_ID,1/*1 log or 0 don't*/,V_DBC_RETURN_CODE,V_DBC_RETURN_MSG,V_DBC_ROWS_COUNT);
@@ -98,6 +210,16 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1P_PP.STG_PROCESS_LOADING
 				LEAVE MainBlock;
 			END IF;	
 			
+			if V_UPDATE_ALL_TO_D <> ''
+			then
+				SET V_SQL_SCRIPT_ID = V_SQL_SCRIPT_ID + 1;
+				CALL GDEV1P_PP.DBC_SYSEXECSQL_WITH_LOG(V_UPDATE_ALL_TO_D,V_SQL_SCRIPT_ID,v_run_id,V_SOURCE_NAME,i_TABLE_NAME,I_LOAD_ID,1/*1 log or 0 don't*/,V_DBC_RETURN_CODE,V_DBC_RETURN_MSG,V_DBC_ROWS_COUNT);
+				IF V_DBC_RETURN_CODE <> 0 THEN
+					SET V_RETURN_CODE = V_DBC_RETURN_CODE;
+					SET V_RETURN_MSG = V_DBC_RETURN_MSG;
+					LEAVE MainBlock;
+				END IF;
+			end if;
 			
 			
 			SET V_SQL_SCRIPT_ID = V_SQL_SCRIPT_ID + 1;
