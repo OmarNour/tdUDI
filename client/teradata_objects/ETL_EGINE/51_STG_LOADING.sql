a3327e60d8e3b06f510fe1f7da2fe3c0710de86f
Omar Nour <omar_nour@hotmail.com>
2023-04-25 15:30:05 +0200

    dev

--- client/teradata_objects/ETL_EGINE/51_STG_LOADING.sql
+++ client/teradata_objects/ETL_EGINE/51_STG_LOADING.sql
@@ -15,7 +15,9 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1_ETL.STG_LOADING
     BEGIN
         DECLARE 	V_RETURN_CODE  				INTEGER DEFAULT 0;
         DECLARE 	V_RETURN_MSG 				VARCHAR(5000) DEFAULT 'Process Completed Successfully';
-        DECLARE 	V_INSERTED_ROWS_COUNT,V_UPDATED_ROWS_COUNT,V_DELETED_ROWS_COUNT FLOAT DEFAULT 0;
+        DECLARE 	V_INSERTED_ROWS_COUNT,V_UPDATED_ROWS_COUNT,V_DELETED_ROWS_COUNT,
+		        	V_dbc_INSERTED_ROWS_COUNT,V_dbc_UPDATED_ROWS_COUNT,V_dbc_DELETED_ROWS_COUNT 
+        			FLOAT DEFAULT 0;
         DECLARE		v_run_id					BIGINT;
         
         DECLARE 	V_DBC_RETURN_CODE 			INTEGER;
@@ -39,6 +41,46 @@ REPLACE  PROCEDURE /*VER.01*/ GDEV1_ETL.STG_LOADING
 				SET v_run_id=	i_run_id;
 			END IF;
             
+			FOR	loop1 AS loopx CURSOR FOR 
+						SELECT 
+					    TBL.TABLE_NAME
+					    FROM GDEV1_ETL.STG_TABLES TBL
+					    WHERE EXISTS (
+					    SELECT 1
+					        FROM GDEV1_ETL.SOURCE_SYSTEMS SRC
+					        WHERE TBL.SOURCE_NAME=SRC.SOURCE_NAME
+					            AND  SRC.ACTIVE = 1
+					            AND  SRC.STG_ACTIVE = 1
+					            AND  SRC.ACTIVE = 1
+					    )
+					        AND  TBL.ACTIVE = 1
+					        AND  (TBL.TABLE_NAME = i_TABLE_NAME or i_TABLE_NAME is  null )
+					        AND  (TBL.SOURCE_NAME = i_SOURCE_NAME or i_SOURCE_NAME is  null )
+					        
+			DO
+				call GDEV1_ETL.STG_PROCESS_LOADING
+											    (
+											    loop1.TABLE_NAME	--IN 		I_PROCESSNAME  			VARCHAR(1000),
+											    ,v_run_id			--IN 		I_RUN_ID 				BIGINT,
+											    ,I_LOAD_ID			--IN		I_LOAD_ID				VARCHAR(500),
+											    ,I_RELOAD			--IN 		I_RELOAD,	
+											    ,I_ALL_BATCHES		--IN 		I_ALL_BATCHES,	
+											    ,V_dbc_INSERTED_ROWS_COUNT 	--OUT  	O_ROWS_INSERTED_COUNT	FLOAT,
+											    ,V_dbc_updated_ROWS_COUNT 	--OUT  	O_ROWS_UPDATED_COUNT	FLOAT,
+											    ,V_dbc_deleted_ROWS_COUNT 	--OUT  	O_ROWS_DELETED_COUNT	FLOAT,
+											    ,V_DBC_RETURN_CODE 	--OUT  	O_RETURN_CODE  			INTEGER,
+											    ,V_DBC_RETURN_MSG 	--OUT  	O_RETURN_MSG 			VARCHAR(5000)
+											    );
+				if v_dbc_return_code <> 0 and V_RETURN_CODE = 0
+				then
+					set V_RETURN_CODE = v_dbc_return_code;
+					set V_RETURN_MSG = V_DBC_RETURN_MSG;
+				end if;
+				
+				set V_INSERTED_ROWS_COUNT = V_INSERTED_ROWS_COUNT + V_dbc_INSERTED_ROWS_COUNT;
+				set V_updated_ROWS_COUNT = V_updated_ROWS_COUNT + V_dbc_updated_ROWS_COUNT;
+				set V_deleted_ROWS_COUNT = V_deleted_ROWS_COUNT + V_dbc_deleted_ROWS_COUNT;
+			end for;
         END;
                                                                                         
                                                                                          
