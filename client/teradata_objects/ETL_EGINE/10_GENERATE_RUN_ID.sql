3f925a89da0a4b7be575be564038cb448342830b
Omar Nour <omar_nour@hotmail.com>
2023-06-28 17:55:30 +0300

    dev

--- client/teradata_objects/ETL_EGINE/10_GENERATE_RUN_ID.sql
+++ client/teradata_objects/ETL_EGINE/10_GENERATE_RUN_ID.sql
@@ -9,4 +9,7 @@ REPLACE FUNCTION GDEV1_ETL.generate_run_id ()
     COLLATION INVOKER
     INLINE TYPE 1
     
+    -- Function to generate a unique run ID based on the current timestamp
+    -- and removing any non-numeric characters
+    
     RETURN CAST(oreplace(oreplace(oreplace(oreplace(oreplace( CAST(CURRENT_TIMESTAMP(6) AS VARCHAR(24)),'-',''),' ',''),':',''),'.',''),'+','')AS BIGINT);
